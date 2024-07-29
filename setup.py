4aa5c61b3a898a55717ea5eb550e8bac5964c10f
shreehari-divate <shreeharidivate03@gmail.com>
2024-07-29 11:39:59 +0530

    Added setup file

---
+++ setup.py
@@ -0,0 +1,20 @@
+from typing import List
+from setuptools import find_packages,setup
+
+HYPEN_E_DOT='-e.'
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as f:
+        requirements=f.readlines()
+        requirements=[req.replace("\n","")for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+setup(
+    name="Machine-Failure",
+    version="0.0.1",
+    author="Shreehari",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt")
+)        
